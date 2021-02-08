bf60834bfef4a7e46a6397c7a58598f3fd0b947a
Jacques Sarraffe <jsarraffe@gmail.com>
2021-02-08 14:17:14 -0800

    Initial Commit

---
+++ MysticSqaure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MysticSqaure
+//
+//  Created by Jacques Sarraffe on 2/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MysticSqaure/MysticSqaureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MysticSqaureApp.swift
+//  MysticSqaure
+//
+//  Created by Jacques Sarraffe on 2/8/21.
+//
+
+import SwiftUI
+
+@main
+struct MysticSqaureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
